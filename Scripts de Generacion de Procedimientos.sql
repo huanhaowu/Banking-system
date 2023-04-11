bd24d90b62300319a9920b95338c3b43478b8cef
Huan Hao Wu <huanhaowu28@gmail.com>
2023-04-11 15:50:54 -0400

    Creacion Archivo Procedimientos Almacenados
    Se creo el archivo que contendrá todos los procedimientos almacenados que se estarán utilizando en la base de datos del banco

---
+++ Scripts de Generacion de Procedimientos.sql
@@ -0,0 +1,22 @@
+-----------------------------PRESENTACION----------------------------------------
+-- Base de Datos de un Banco
+
+-- * Rolbik Urbaez 1105721
+
+-- * Nelson Ureña 1105268
+
+-- * Cristofers Valdez 1104369
+
+-- * Huan Hao Wu 1104326 
+
+-- * Jin Cheng Wu 1106398
+
+-- * Natanael Zorrilla 1103111
+
+---------------------------------------------------------------------------------
+
+
+USE SistemaBancario;
+GO
+
+------------------------- PROCEDIMIENTOS ALMACENADOS ----------------------------
