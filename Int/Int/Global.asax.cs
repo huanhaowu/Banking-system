bc37c18acd6e523f00540711eac97107eff8aee9
cristofers <valdezcristofers@gmail.com>
2023-04-20 17:01:30 -0400

    registrar log4net en base de datos

--- Int/Int/Global.asax.cs
+++ Int/Int/Global.asax.cs
@@ -7,15 +7,18 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using log4net;
 
 namespace Int
 {
     public class Global : HttpApplication
     {
+        private static readonly ILog Logger = LogManager.GetLogger(System.Environment.MachineName);
+
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-            log4net.Config.XmlConfigurator.Configure();
+            Logger.Info("Testing information log");
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);            
--- Int/Int/Properties/AssemblyInfo.cs
+++ Int/Int/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ea028fa8-9655-438d-94b1-1a962e1ba1c3")]
-
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "Web.config", Watch = true)]
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
