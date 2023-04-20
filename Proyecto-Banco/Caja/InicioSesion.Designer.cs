3ac38b4d542bf937232d8ea535903fc3a40eec8a
Jin Cheng Wu Feng <1017745312qq@gmail.com>
2023-04-20 03:00:18 -0400

    Creacion Pantalla de InicioSesion

--- Proyecto-Banco/Caja/InicioSesion.Designer.cs
+++ Proyecto-Banco/Caja/InicioSesion.Designer.cs
@@ -40,9 +40,10 @@
             this.lblIniciarSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(182)))));
             this.lblIniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblIniciarSesion.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblIniciarSesion.Location = new System.Drawing.Point(562, 865);
+            this.lblIniciarSesion.Location = new System.Drawing.Point(422, 703);
+            this.lblIniciarSesion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblIniciarSesion.Name = "lblIniciarSesion";
-            this.lblIniciarSesion.Size = new System.Drawing.Size(315, 42);
+            this.lblIniciarSesion.Size = new System.Drawing.Size(259, 36);
             this.lblIniciarSesion.TabIndex = 0;
             this.lblIniciarSesion.Text = "INICIAR SESION";
             this.lblIniciarSesion.Click += new System.EventHandler(this.label1_Click);
@@ -52,9 +53,10 @@
             this.txtUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
             this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsuario.Location = new System.Drawing.Point(510, 491);
+            this.txtUsuario.Location = new System.Drawing.Point(545, 506);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(413, 54);
+            this.txtUsuario.Size = new System.Drawing.Size(310, 43);
             this.txtUsuario.TabIndex = 1;
             this.txtUsuario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -63,20 +65,22 @@
             this.txtClave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(240)))), ((int)(((byte)(248)))));
             this.txtClave.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtClave.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtClave.Location = new System.Drawing.Point(510, 623);
+            this.txtClave.Location = new System.Drawing.Point(382, 506);
+            this.txtClave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtClave.Name = "txtClave";
-            this.txtClave.Size = new System.Drawing.Size(413, 54);
+            this.txtClave.Size = new System.Drawing.Size(310, 43);
             this.txtClave.TabIndex = 2;
             // 
             // InicioSesion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1422, 993);
+            this.ClientSize = new System.Drawing.Size(1028, 609);
             this.Controls.Add(this.txtClave);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.lblIniciarSesion);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "InicioSesion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "InicioSesion";
